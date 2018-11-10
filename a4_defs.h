ccb01e5ae8208a5fff1c33f64bb050027d62b6c0
Dustin Fast <dustin.fast@outlook.com>
2018-11-09 17:53:15 -0900

    Added MemBlock struct -DF

--- a4_defs.h
+++ a4_defs.h
@@ -4,6 +4,18 @@ a4_fs definitions - A collection of definitions and helper funcs for a4_fs.c
 Contributors: Dustin Fast
 */
 
+// #include <stddef.h>
+// #include <sys/stat.h>
+// #include <sys/statvfs.h>
+// #include <stdint.h>
+// #include <string.h>
+#include <time.h>
+// #include <stdlib.h>
+// #include <sys/types.h>
+// #include <unistd.h>
+// #include <fcntl.h>
+// #include <errno.h>
+// #include <stdio.h>
 
 /* Begin Definitions ---------------------------------------------------- */
 
@@ -11,54 +23,41 @@ Contributors: Dustin Fast
 #define BYTES_IN_KB (1024)                  // Num bytes in a kb
 #define BLOCK_SZ_BYTES (4 * BYTES_IN_KB)    // block size, in bytes
 #define FNAME_MAXLEN (256)                  // Max length of any filename
+#define MAGIC_NUM ((uint32_t) (UINT32_C(0xdeadd0c5)))
 
-/* File system's I-Node struct -
-    Files have a inode numbers assigned to them. 
-    Folders are a list of file objects that have there associated inodes.*/
+/* Inode struct -
+An Inode is a file or directory header. Each contains file/dir attributes
+and ptrs to either a File or Dir obejct instance. */
 typedef struct Inode { 
-    int inode_number;       // This Inode's index
-    void *block_start;      // Pointer to start address of virtual mem block 
-    int is_free;            // Denotes block is free
-    struct Inode *next;     // Next Inode
+    char filename[FNAME_MAXLEN];  // This file's (or dir's) label
+    int is_dir;                   // 1 = node reprs a dir, else reprs a file
+    size_t max_size_bytes;        // Max size (before grow needed)
+    size_t curr_size_bytes;       // Currently used space (of max_size)
+    struct timespec last_access;  // Last access time
+    struct timespec last_mod;     // Last modified time
+    void *first_block;            // Ptr to first MemBlock used by the file/dir
 } Inode ;
 
-/* File system's File struct -
-    Files are the filesystem's main container, used to represent either an
-    individual file (each w/an inode # assigned) or a folder of files (each
-    folder is simply a list of Files). */
-typedef struct File {
-    char filename[FNAME_MAXLEN];  // This file's (or folder's) name
-    void *starting_inode;   // First inode (we should keep inodes consecutive)
-    size_t filesize;        // File length in KB (must be 4KB block sz aligned)
-    int is_dir;             // Denotes this File is a directory
-    struct File *next;      // The next file in the folder (or first if is_dir)
-} File;
+/* Memory block struct -
+Each file and/or folder uses a number of memory blocks. We keep track of free
+blocks with FileSystem->free_blocks. */
+typedef struct MemBlock {
+    size_t size_bytes;      // Mem block size (BLOCK_SZ_BYTES aligned)
+    size_t payload_addr;    // Start of payload memory field
+    size_t payload_size;    // Size of data in payload field
+    struct MemBlock *next;  // The next mem block used by the file/dir, if any
+} MemBlock;
 
 /* The Filesystem struct
-    A file system is a list of inodes, whith each node pointing to the address
-    blocks in memory used by each file. */
+    A file system is a list of inodes with each node pointing to the memory 
+    address used by the file/dir the inode represents. */
 typedef struct FileSystem {
-    size_t size;                // Total filesystem memory space, in bytes
-    Inode *head;                // Ptr to fs's head node / 1st mem block addr
-    Inode *first_free;          // Ptr to the "free nodes" list. NEEDED?
-    File *root;                 // Ptr to filesystem's root dir (a File obj)
+    size_t size;            // Total filesystem space in bytes incl this header
+    Inode *root;            // Ptr to file system's root directory inode
+    Inode *free_blocks;     // Ptr to the "free mem blocks" list.
 } FileSystem;
 
-// Memory block structure - adapted from clauter's in-class lecture (DF):
-// struct __myfs_memory_block_t {  // MemBlock
-//       size_t      size;
-//       size_t      user_size;
-//       __myfs_off_t next;
-// } __myfs_memory_block_t;
-
-// // Filesystem handle structure - adapted from clauter's in-lass lecture (DF):
-// struct __myfs_handle_t {      // FSHandle
-//       uint32_t    magic;
-//       __myfs_off_t free_memory;
-//       __myfs_off_t root dir;
-//       size_t       size;
-// } __myfs_handle_t;
-
+// Sizes of the above structs, for convenience
 #define INODE_OBJ_SZ sizeof(Inode)    // Size of the Inode struct (bytes)
 #define FILE_OBJ_SZ sizeof(File)      // Size of File struct (bytes)
 #define FS_OBJ_SZ sizeof(FileSystem)  // Size of FileSystem struct (bytes)
